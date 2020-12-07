2ab55ece6410b3c01c6fe946ae42443451dd3ef5
Prabhu Srikantan <prabhu@aum-services.co.uk>
2020-12-07 18:47:04 +0000

    Next update

--- test.js
+++ test.js
@@ -1,15 +1,8 @@
 const assert = require("assert");
 const _ = require("lodash");
 const moment = require("moment");
+const math = require("mathjs");
 var rawEvents = require("./raw_events.json");
-
-describe("Array", function () {
-    describe("#indexOf()", function () {
-        it("should return -1 when not found", function () {
-            assert.strictEqual([1,2,3,].indexOf(4), -1);
-        });
-    });
-});
 
 describe("Events Tests", function () {
     // Prepare dates
@@ -20,15 +13,30 @@ describe("Events Tests", function () {
     describe("number of events", function () {
         it("output events count", function () {
             var groupedByDate = _.groupBy(rawEvents, 'date');
-            var prevValue = 0;
+            var prevValue = -1;
             _.forEach(_.keys(groupedByDate), function (k) {
-                if(prevValue !== 0) {
+                console.log(`Number of events on ${k} ==> ${groupedByDate[k].length}`)
+                if(prevValue !== -1) {
                     if(!((prevValue * 0.8) <= groupedByDate[k].length && groupedByDate[k].length <= (prevValue * 1.2))) {
                         console.log(`Number of events for ${k} varies by more than 20% from previous day's data. Previous day's value => ${prevValue}. Current value => ${groupedByDate[k].length}`);
                     }
                 }
                 prevValue = groupedByDate[k].length;
             });
+        });
+    });
+
+    describe("null values", function () {
+        it("null user names", function () {
+            var userNulls = _.filter(rawEvents, function (event) {
+                return event.username == null || event.username == "null" || !event.username;
+            });
+
+            _.forEach(userNulls, function (event) {
+                console.log(`User name is null for ID : ${event._id}, Hook: ${event.hookname}, Hook Id : ${event.hookid}`);
+            });
+
+            console.log("Numer of events that have no user id => ", userNulls.length);
         });
     });
 });
